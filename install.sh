a8da63391a3c2275a9c60300597f11ab3b640fd8
Ioannis Begleris <i.begleris@soton.ac.uk>
2016-06-07 14:26:26 +0000

    fixed the install file

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ sudo dist-upgrade
 pip install --upgrade scipy
 echo "sorry for the sudo but gmsh2.12 needs to be installed"
 wget http://gmsh.info/bin/Linux/gmsh-2.12.0-Linux64.tgz
-tar zxvf gmsh-2.10.1-Linux64.tgz
-sudo cp gmsh-2.10.1-Linux/bin/gmsh /usr/bin/
-sudo cp -r gmsh-2.10.1-Linux/share/doc/gmsh /usr/share/doc/
-sudo cp gmsh-2.10.1-Linux/share/man/man1/gmsh.1 /usr/share/man/man1/
+tar zxvf gmsh-2.12.0-Linux64.tgz
+sudo cp gmsh-2.12.0-Linux/bin/gmsh /usr/bin/
+sudo cp -r gmsh-2.12.0-Linux/share/doc/gmsh /usr/share/doc/
+sudo cp gmsh-2.12.0-Linux/share/man/man1/gmsh.1 /usr/share/man/man1/
 rm gmsh-2.12.0-Linux64.tgz
 
